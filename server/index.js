a95899918052e66989b432f2412dddc1a06fc0b0
mpiastucki <michael.piastucki@woven-planet.global>
2022-09-14 11:26:50 +0900

    return Static files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
+const path = require('path')
+const fs = require('fs');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve(__dirname, "../build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../build", "index.html"))
+})
+
+app.get("/api/login", (req, res)=>{
+    
+})
 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`)
